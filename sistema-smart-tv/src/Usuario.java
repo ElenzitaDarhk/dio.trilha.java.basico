41b04d2ff6e29c2d4818acd2ac8d4d5f20b0f33b
Elen <elencp87@gmail.com>
2025-04-25 10:07:06 -0300

    feat: tratamento de exceção

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,6 +1,7 @@
 
-import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Usuario {
 
@@ -65,15 +66,21 @@ public class Usuario {
 
         System.out.println("Canal via entrada de prompt");
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite o canal desejado!");
-        canal = scanner.nextInt();
-        smartTv.escolherCanal(canal);
+            System.out.println("Digite o canal desejado!");
+            canal = scanner.nextInt();
+            smartTv.escolherCanal(canal);
 
-        System.out.println("TV Ligada? " + (smartTv.verificarLigada() ? "Ligada" : "Desligada"));
-        System.out.println("Canal: " + smartTv.getCanal());
-        System.out.println("Volume: " + smartTv.getVolume());
+            System.out.println("TV Ligada? " + (smartTv.verificarLigada() ? "Ligada" : "Desligada"));
+            System.out.println("Canal: " + smartTv.getCanal());
+            System.out.println("Volume: " + smartTv.getVolume());
 
+        } catch (InputMismatchException e) {
+            System.out.println("Canal inválido!");
+            System.out.println("Digite um número inteiro!");
+
+        }
     }
 }
